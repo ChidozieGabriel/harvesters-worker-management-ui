b927c3cc7a12c220b39344f5d601a0059cf424ad
Chidozie Nnabugwu <nnabugwuchidozie@gmail.com>
2025-06-07 14:02:17 +0100

    Create toaster

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { BrowserRouter as Router } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from 'react-hot-toast';
+import { BrowserRouter as Router } from 'react-router-dom';
 import AppRoutes from './routes';
 
 
@@ -11,7 +12,7 @@ function App() {
       <Router>
         <div className="min-h-screen bg-gray-50">
           <AppRoutes />
-          {/* <Toaster /> */}
+          <Toaster />
         </div>
       </Router>
     </QueryClientProvider>
